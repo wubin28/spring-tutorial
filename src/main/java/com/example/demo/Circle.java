307fde65ebe92c1486554d1d8d15b1c35a63af72
Ben WU <wubin28@gmail.com>
2018-12-04 19:28:47 +0800

    remove getters and setters and default constructors

--- src/main/java/com/example/demo/Circle.java
+++ src/main/java/com/example/demo/Circle.java
@@ -1,15 +1,8 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Required;
-
 public class Circle implements Shape {
     private Point center;
 
-    public Point getCenter() {
-        return center;
-    }
-
-    @Required
     public void setCenter(Point center) {
         this.center = center;
     }
--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -6,36 +6,9 @@ public class Triangle implements Shape {
     private Point pointB;
     private Point pointC;
 
-    public Triangle() {
-    }
-
     public Triangle(Point pointA, Point pointB, Point pointC) {
         this.pointA = pointA;
         this.pointB = pointB;
-        this.pointC = pointC;
-    }
-
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
         this.pointC = pointC;
     }
 

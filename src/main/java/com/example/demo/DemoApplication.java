d3a84481baf92a50621fe71067d43f01908646d6
Ben WU <wubin28@gmail.com>
2018-12-04 11:16:16 +0800

    remove spring boot application annotation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {

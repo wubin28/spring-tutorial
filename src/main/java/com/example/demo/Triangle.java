0bda0eadbb36c500511cc431ed6f1e05cf344379
Ben WU <wubin28@gmail.com>
2018-12-06 10:15:00 +0800

    06 Using Constructor Injection - add 2 constructor args

--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -3,12 +3,18 @@ package com.example.demo;
 
 public class Triangle {
     private String type;
+    private int height;
 
     public Triangle(String type) {
         this.type = type;
     }
 
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
     public String draw() {
-        return "Drawing triangle whose type is " + type;
+        return "Drawing triangle whose type is " + type + " and height is " + height;
     }
 }
